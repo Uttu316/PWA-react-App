9e4170cafc275f5504e994b12d851222f57c2951
Uttu316 <34169292+Uttu316@users.noreply.github.com>
2019-10-14 18:19:56 -0400

    first installable pwa

---
+++ build/precache-manifest.20d19e5c9ebaf07105fc6860dfceacd6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31ae8e65c3175aaaf35fbdff834bb731",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca965bd8c63054bfef33",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "8a63d0c69e9ab9b8b70f",
+    "url": "/static/js/2.1632cf1e.chunk.js"
+  },
+  {
+    "revision": "ca965bd8c63054bfef33",
+    "url": "/static/js/main.f62ac620.chunk.js"
+  },
+  {
+    "revision": "83d97cf838c74a71b465",
+    "url": "/static/js/runtime-main.ab3ec558.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
